97ae7a6d0ba949dc9e0e1ba026ee45eb6798374f
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-09 21:31:00 -0300

    Metodo saque concluido

--- python/sistema_bancario/main.py
+++ python/sistema_bancario/main.py
@@ -10,26 +10,32 @@ import operacoes
 
 # variáveis
 saldo = float(0)
+quantidade_saque = int(0)
 # constantes
 LIMITE = int(500)
 LIMITE_SAQUE = int(3)
 
 while True:
     # Operações solicitadas para o sistema
-    menu = str('''  
-    Operações:
+    menu = str(f'''
+    OPERAÇÕES:
 
         [1] - Depositar
         [2] - Sacar
         [3] - Extrato
         [4] - Sair
-            
+               
+        SALDO DÍSPONIVEL: {saldo:.2f}
+        SAQUES EFETUADOS: {quantidade_saque}
+
         : ''')
     opcao = str(input(menu))
 
     match opcao:
         case '1':
             saldo = operacoes.deposito(saldo)
+        case '2':
+            saldo, quantidade_saque = operacoes.saque(saldo, quantidade_saque, LIMITE)
 
         case '4':
             break
--- python/sistema_bancario/operacoes.py
+++ python/sistema_bancario/operacoes.py
@@ -1,5 +1,13 @@
-# modulos contendo todos os métodos de operações: deposito; saque; extrato
+'''
+modulos contendo todos os métodos de operações:
+    1) deposito; 
+    2) saque; 
+    3) extrato
+'''
+
+# variáveis
 lista_de_depositos = list()
+lista_de_saques = list()
 
 
 def deposito(saldo_atual):
@@ -18,6 +26,25 @@ def deposito(saldo_atual):
     return saldo_atual
 
 
-
-def saque(saldo_atual):
+def saque(saldo_atual, quantidade_saque, LIMITE):
     print('SAQUE')
+
+    # veficar se pode efetuar saque
+    if quantidade_saque == 3:
+        print('Limite de saques diário atingido.')
+        return saldo_atual, quantidade_saque
+
+    valor_saque = float(input('Digite o valor do saque (max: R$ 500,00): '))
+
+    # verificando se o valor do saque é valido
+    if (valor_saque <= saldo_atual) and (valor_saque <= LIMITE):
+        saldo_atual -= valor_saque
+        # lista de extrato com todos os saques
+        lista_de_saques.append(valor_saque)
+        quantidade_saque += 1
+        print(f'R$ {valor_saque:.2f} sacados')
+
+    else:
+        print('Valor inválido')
+
+    return saldo_atual, quantidade_saque
