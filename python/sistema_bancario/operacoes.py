0ab7401b6bb867d58ceb52ade941b0578ecefa91
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-09 21:39:41 -0300

    Documentando o código

--- python/sistema_bancario/operacoes.py
+++ python/sistema_bancario/operacoes.py
@@ -1,8 +1,18 @@
 '''
 modulos contendo todos os métodos de operações:
-    1) deposito; 
+    1) deposito;
+        qualquer valor positivo
+
     2) saque; 
+        3 saques
+        R$ 500 maximo
+        retorna 2 valores
+            1. saldo
+            2. quantidade de saque
+
     3) extrato
+        Lista de saques
+        Lista de depositos
 '''
 
 # variáveis
@@ -24,6 +34,11 @@ def deposito(saldo_atual):
         print('Valor inválido')
 
     return saldo_atual
+    """
+    Deve ser possível depositar valores positivos para a minha conta bancária. 
+    A v1 do projeto trabalha apenas com 1 usuário, dessa forma não precisamos nos preocupar em identificar qual é o número da agência e conta bancária. 
+    Todos os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
+    """
 
 
 def saque(saldo_atual, quantidade_saque, LIMITE):
@@ -48,3 +63,19 @@ def saque(saldo_atual, quantidade_saque, LIMITE):
         print('Valor inválido')
 
     return saldo_atual, quantidade_saque
+    """
+    O sistema deve permitir realizar 3 saques diários com limite máximo de R$ 500,00 por saque. 
+    Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por falta de saldo. 
+    Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato.
+    """
+
+
+def extrato():
+    return
+    """
+    Essa operação deve listar todos os depósitos e saques realizados na conta. No fim da listagem deve ser exibido o saldo atual da conta. 
+    Se o extrato estiver em branco, exibir a mensagem: Não foram realizadas movimentações.
+
+    Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo:
+        1500,45 -- R$ 1500.45
+    """
