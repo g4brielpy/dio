27b924333c15b7dfaad3ec985febab333eeb85e0
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-11 16:56:16 -0300

    Otimizaçõa do sistema

--- python/sistema_bancario/operacoes.py
+++ python/sistema_bancario/operacoes.py
@@ -38,7 +38,11 @@ def deposito(saldo_atual):
     Deve ser possível depositar valores positivos para a minha conta bancária. 
     A v1 do projeto trabalha apenas com 1 usuário, dessa forma não precisamos nos preocupar em identificar qual é o número da agência e conta bancária. 
     Todos os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+    Otimização:
+    utilizar somente argumentos por 'posição'
     """
+    
 
 
 def saque(saldo_atual, quantidade_saque, LIMITE):
@@ -70,6 +74,9 @@ def saque(saldo_atual, quantidade_saque, LIMITE):
     O sistema deve permitir realizar 3 saques diários com limite máximo de R$ 500,00 por saque. 
     Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por falta de saldo. 
     Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+    Otimização:
+    utilizar somente argumentos por 'nomes'
     """
 
 
@@ -93,4 +100,7 @@ def extrato():
 
     Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo:
         1500,45 -- R$ 1500.45
+
+    Otimização:
+    utilizar argumentos por 'posição e nome'
     """
