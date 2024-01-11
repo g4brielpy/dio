bc282650987695bb8f26b0dfca9acb81f58b6bc9
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-11 17:20:44 -0300

    Módulo criar conta e usuário

---
+++ python/sistema_bancario/user.py
@@ -0,0 +1,12 @@
+'''
+. Criar usuário
+    O programa deve armazenar os usuários em uma lista, um usuário é composto por: nome, data de nascimento, cpf e endereço. 
+    O endereço é uma string com o formato: logradouro, nro bairro cidade/sigla estado. (não e obrigatório validar formato)
+    Deve ser armazenado somente os números do CPF(Sem '.', '-'). 
+    Não podemos cadastrar 2 usuários com o mesmo CPF.
+
+. Cria conta corrente
+    O programa deve armazenar contas em uma lista, uma conta é composta por: agência, número da conta e usuário. 
+    O número da conta é sequencial, iniciando em 1. O número da agência é fixo: "0001". 
+    O usuário pode ter mais de uma conta, mas uma conta pertence a somente um usuário.
+'''
